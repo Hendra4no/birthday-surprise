a49b536c6817137ab0a1d0ab59345900e2de3a1a
Tanner Welsh <tannerwelsh@gmail.com>
2014-04-03 18:54:55 -0400

    Replace video on homepage with something else. ;D

--- js/video_player.js
+++ js/video_player.js
@@ -1,10 +1,15 @@
-(function(){var div="\
+(function() {
+  var div="\
     <div id='video-player'>\
       <div class='video-box'>\
         <div class='close'><a href='#'>CLOSE</a></div>\
         <div class='video'></div>\
       </div>\
     </div>"
-$('body').on('click','#video-player',function(event){event.preventDefault();$('#video-player').remove();});function VideoPlayer(oembed){this.iframe=oembed.html;}
-VideoPlayer.prototype.show=function(){$('body').append(div);$('#video-player .video').html(this.iframe);}
-this.VideoPlayer=VideoPlayer;})()
+  $('body').on('click','.play',function(event){
+    event.preventDefault();
+
+    $('.intro-video').html('');
+    $('.intro-video').append('<iframe width="640" height="480" src="http://www.youtube.com/embed/dsE6D-Xg-Ns?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>');
+  });
+})()
